f25cd555bfea88b6659078fb83dff656912acbe1
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-10-22 13:12:11 -0300

    commit

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'meu-cache-v1'
+const CACHE_FILES = [
+    // Informar os arquivos que estáticos que ficarão disponíveis offline
+    '/',
+]
+
+// Coloca seus arquivos no CACHE do navegador web
+self.addEventListener("install", (evento) => {
+    evento.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then((cache) => cache.addAll(CACHE_FILES))
+        .then(() => self.skipWaiting())
+    );
+  });
+  
+  // Remove cache antigo, caso exista
+  self.addEventListener("activate", function (evento) {
+    evento.waitUntil(
+      caches.keys().then(function (cacheNames) {
+        return Promise.all(
+          cacheNames.map(function (thisCacheName) {
+            if (thisCacheName !== CACHE_NAME) {
+              return caches.delete(thisCacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
+  // Intercepta a requisição web.
+  // Se tiver em cache, não precisa buscar na internet
+  self.addEventListener("fetch", function (evento) {
+    console.log('Requisição na URL \n', evento.request.url);
+    evento.respondWith(
+      caches
+        .match(evento.request)
+        .then(function (response) {
+          return response || fetch(evento.request);
+        })
+        .catch(function (error) {
+          console.log("[ServiceWorker] fetch error", error);
+        })
+    );
+  });
