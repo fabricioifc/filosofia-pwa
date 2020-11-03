bb40d7e59bad1ea0779b313d4ceec6ec50723986
Fabricio Bizotto <fabricio.bizotto@ifc.edu.br>
2020-11-03 10:44:37 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,19 +3,6 @@ const CACHE_FILES = [
     // Informar os arquivos que estáticos que ficarão disponíveis offline
     '/',
     '/index.html',
-    '/historia.html',
-    '/obras.html',
-
-    '/manifest.json',
-    'https://fonts.googleapis.com/css2?family=Piedra&display=swap',
-    'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
-    'https://fonts.gstatic.com/s/piedra/v9/ke8kOg8aN0Bn7hTem0aG.woff2',
-    'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxK.woff2',
-    '/assets/css/estilo.css',
-    '/assets/imagens/tales.jpg',
-    '/assets/imagens/fundo.png',
-    '/assets/imagens/pensadores.jpg',
-    '/assets/js/main.js',
 ]
 
 // Coloca seus arquivos no CACHE do navegador web
